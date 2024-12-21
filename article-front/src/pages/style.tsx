cb7eafe400373c37cb04d69aa322cef649cd1765
rodolfomno <rodolfo_mno@hotmail.com>
2024-12-20 21:16:04 -0300

    feat: style info page

--- article-front/src/pages/style.tsx
+++ article-front/src/pages/style.tsx
@@ -20,3 +20,33 @@ align-items: center;
 padding-bottom: 20px;
 font-size: 35px;
 `
+
+export const InfoCard = styled.div`
+width: 700px;
+min-height: 400px;
+border: solid 1px black;
+border-radius: 15px;
+display: flex;
+flex-direction: column;
+background-color:rgb(242, 243, 233);
+`
+
+export const ItemRow = styled.div`
+padding: 15px;
+`
+
+export const ItemContent = styled.div`
+`
+
+export const ItemTitle = styled.div`
+font-size: 20px;
+`
+
+export const BackToHomeButton = styled.div`
+margin-top: 15px;
+padding: 12px;
+border: solid 1px black;
+border-radius: 30px;
+cursor: pointer;
+background-color: rgb(211, 215, 245);
+`
